70ee8b313f56f9218b886995038062e6c33f01b4
张明阳 <zhangmingyang@shinhofood.com>
2021-01-04 11:32:02 +0800

    feat: 删除 js 源文件

--- utils/root.js
+++
@@ -1,9 +0,0 @@
-import freeGlobal from './freeGlobal.js'
-
-/** Detect free variable `self`. */
-const freeSelf = typeof self === 'object' && self !== null && self.Object === Object && self
-
-/** Used as a reference to the global object. */
-const root = freeGlobal || freeSelf || Function('return this')() // eslint-disable-line
-
-export default root
